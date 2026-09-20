939874021eed8b6a8829c235fdb901048871a999
Xguiler <guilerxx@gmail.com>
2026-09-20 18:40:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SB_CONFIG = {
   SUPABASE_URL: "https://ketkskvqabxfqgrhkegd.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_bsQ5nXIzAPWK6uIpHpixnw_n7bXTAQw",
-  ROOM_ID: "xguiler-soundboard-caos-control"
+  ROOM_ID: "xguiler-soundboard"
 };
